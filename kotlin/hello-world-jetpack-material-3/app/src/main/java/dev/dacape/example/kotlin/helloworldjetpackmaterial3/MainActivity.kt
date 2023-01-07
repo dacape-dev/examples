b94468f5d3535ee12633b620d871a3ee021362f8
dcarvajal7 <danielcarvajalpelicano@gmail.com>
2023-01-07 20:39:05 +0100

    include text field

--- kotlin/hello-world-jetpack-material-3/app/src/main/java/dev/dacape/example/kotlin/helloworldjetpackmaterial3/MainActivity.kt
+++ kotlin/hello-world-jetpack-material-3/app/src/main/java/dev/dacape/example/kotlin/helloworldjetpackmaterial3/MainActivity.kt
@@ -3,16 +3,24 @@ package dev.dacape.example.kotlin.helloworldjetpackmaterial3
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextRange
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import dev.dacape.example.kotlin.helloworldjetpackmaterial3.ui.theme.HelloWorldTheme
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -22,7 +30,19 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    var text by rememberSaveable(stateSaver = TextFieldValue.Saver) {
+                        mutableStateOf(TextFieldValue("Daniel", TextRange(0, 7)))
+                    }
+
+                    Column(modifier = Modifier.padding(10.dp)) {
+                        TextField(
+                            value = text,
+                            onValueChange = { text = it },
+                            label = { Text("Your name") }
+                        )
+
+                        Greeting(text.text)
+                    }
                 }
             }
         }
@@ -31,7 +51,7 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Text(text = "Hello $name!", )
 }
 
 @Preview(showBackground = true)

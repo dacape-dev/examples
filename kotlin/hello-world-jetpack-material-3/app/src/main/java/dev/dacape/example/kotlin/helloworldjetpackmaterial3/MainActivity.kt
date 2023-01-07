22defe71533d518c2a5b825a1f1a71718ca91580
dcarvajal7 <danielcarvajalpelicano@gmail.com>
2023-01-08 00:45:57 +0100

    hello world with text field composable

--- kotlin/hello-world-jetpack-material-3/app/src/main/java/dev/dacape/example/kotlin/helloworldjetpackmaterial3/MainActivity.kt
+++ kotlin/hello-world-jetpack-material-3/app/src/main/java/dev/dacape/example/kotlin/helloworldjetpackmaterial3/MainActivity.kt
@@ -3,15 +3,14 @@ package dev.dacape.example.kotlin.helloworldjetpackmaterial3
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.TextRange
 import androidx.compose.ui.text.input.TextFieldValue
@@ -20,7 +19,7 @@ import androidx.compose.ui.unit.dp
 import dev.dacape.example.kotlin.helloworldjetpackmaterial3.ui.theme.HelloWorldTheme
 
 class MainActivity : ComponentActivity() {
-    @OptIn(ExperimentalMaterial3Api::class)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -30,34 +29,38 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    var text by rememberSaveable(stateSaver = TextFieldValue.Saver) {
-                        mutableStateOf(TextFieldValue("Daniel", TextRange(0, 7)))
-                    }
-
-                    Column(modifier = Modifier.padding(10.dp)) {
-                        TextField(
-                            value = text,
-                            onValueChange = { text = it },
-                            label = { Text("Your name") }
-                        )
-
-                        Greeting(text.text)
-                    }
+                    HelloWorldWithTextField()
                 }
             }
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!", )
+fun HelloWorldWithTextField() {
+
+    var text by rememberSaveable(stateSaver = TextFieldValue.Saver) {
+        mutableStateOf(TextFieldValue("Daniel", TextRange(0, 7)))
+    }
+
+    Column(modifier = Modifier.padding(10.dp)) {
+        TextField(
+            value = text,
+            onValueChange = { text = it },
+            label = { Text("Your name") }
+        )
+
+        Box(modifier = Modifier.fillMaxWidth()){
+            Text(text = "Hello " + text.text, modifier = Modifier.align(Alignment.Center))
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     HelloWorldTheme {
-        Greeting("Android")
+        HelloWorldWithTextField()
     }
 }

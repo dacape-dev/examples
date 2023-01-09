036871682df4686062503ecb0b6daa8b40027263
dcarvajal7 <danielcarvajalpelicano@gmail.com>
2023-01-09 11:42:36 +0100

    initial import

---
+++ kotlin/navigationjetpack/app/src/main/java/dev/dacape/example/kotlin/navigationjetpack/MainActivity.kt
@@ -0,0 +1,43 @@
+package dev.dacape.example.kotlin.navigationjetpack
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import dev.dacape.example.kotlin.navigationjetpack.ui.theme.NavigationjetpackTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            NavigationjetpackTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    NavigationjetpackTheme {
+        Greeting("Android")
+    }
+}

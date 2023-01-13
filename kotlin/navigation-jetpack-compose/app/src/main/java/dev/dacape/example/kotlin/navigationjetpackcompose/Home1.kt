1bf82e0aa2a7f48cb3cb58d4438f3c39c737368a
dcarvajal7 <danielcarvajalpelicano@gmail.com>
2023-01-13 19:30:24 +0100

    navigation to home2

--- kotlin/navigation-jetpack-compose/app/src/main/java/dev/dacape/example/kotlin/navigationjetpackcompose/Home1.kt
+++ kotlin/navigation-jetpack-compose/app/src/main/java/dev/dacape/example/kotlin/navigationjetpackcompose/Home1.kt
@@ -1,13 +1,17 @@
 package dev.dacape.example.kotlin.navigationjetpackcompose
 
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 
 @Composable
-fun Home1(modifier: Modifier) {
-    Box(modifier = modifier) {
+fun Home1(modifier: Modifier, onNavigateToHome2: () -> Unit) {
+    Column(modifier = modifier) {
         Text("Home1")
+        Button(onClick = onNavigateToHome2) {
+            Text("Go Home2")
+        }
     }
 }
--- kotlin/navigation-jetpack-compose/app/src/main/java/dev/dacape/example/kotlin/navigationjetpackcompose/navigation/AppNavigationHost.kt
+++ kotlin/navigation-jetpack-compose/app/src/main/java/dev/dacape/example/kotlin/navigationjetpackcompose/navigation/AppNavigationHost.kt
@@ -1,5 +1,6 @@
 package dev.dacape.example.kotlin.navigationjetpackcompose.navigation
 
+import android.util.Log
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -30,10 +31,14 @@ fun AppNavigationHost(navController: NavHostController, modifier: Modifier) {
     }
 }
 
+
 fun NavGraphBuilder.homeGraph(navController: NavController, modifier: Modifier) {
+
     navigation(startDestination = "home1", route = AppNavigationBarItems.Home.route) {
         composable("home1") {
-            Home1(modifier)
+            Home1(modifier, onNavigateToHome2 = {
+                navController.navigate("home2")
+            })
         }
         composable("home2") {
             Home2(modifier)

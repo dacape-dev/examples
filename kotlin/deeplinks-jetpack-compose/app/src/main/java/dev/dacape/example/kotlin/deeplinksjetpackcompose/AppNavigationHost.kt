f57e08b47dfd533ef75d59cf46e7bd7a6952bf9e
dcarvajal7 <danielcarvajalpelicano@gmail.com>
2023-01-21 13:12:29 +0100

    deeplink support

--- kotlin/deeplinks-jetpack-compose/app/src/main/java/dev/dacape/example/kotlin/deeplinksjetpackcompose/AppNavigationHost.kt
+++ kotlin/deeplinks-jetpack-compose/app/src/main/java/dev/dacape/example/kotlin/deeplinksjetpackcompose/AppNavigationHost.kt
@@ -1,11 +1,14 @@
 package dev.dacape.example.kotlin.deeplinksjetpackcompose
 
+import android.content.Intent
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import androidx.navigation.navDeepLink
 
 @Composable
@@ -21,7 +24,14 @@ fun AppNavigationHost(
     ) {
         composable(
             "home/{name}",
-            deepLinks = listOf(navDeepLink { uriPattern = "www.example.com/{name}" })
+            deepLinks = listOf(navDeepLink {
+                uriPattern = "https://www.dacape.dev/examples/kotlin/deeplinks-jetpack-compose/{name}"
+                action = Intent.ACTION_VIEW
+            }),
+            arguments = listOf(navArgument("name") {
+                type = NavType.StringType
+                defaultValue = "Dacape"
+            })
         ) {
             backStackEntry ->
                 Home(backStackEntry.arguments?.getString("name"))
